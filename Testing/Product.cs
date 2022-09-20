86a900fb9c020cfdb82852a01ebea22105410348
jose sanchez <jose.sanchez.artist@gmail.com>
2022-09-20 07:22:05 -0700

    create product added

--- Testing/Product.cs
+++ Testing/Product.cs
@@ -1,4 +1,7 @@
-﻿namespace Testing
+﻿using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing
 {
     public class Product
     {
@@ -8,6 +11,7 @@
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
         public int StockLevel  { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
 
     }
 }
